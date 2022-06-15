1e2c4c998ebffef4ec358aaeee37e2bccc59a102
Eric Stanley <ericstanley@pop-os.localdomain>
2022-06-15 23:45:35 +0530

    change AUTOINCREMENT to SERIAL in migrations

--- migrations/1655226837451-initial-schema.js
+++ migrations/1655226837451-initial-schema.js
@@ -5,13 +5,13 @@ module.exports = class initialSchema1655226837451 {
 
   async up(queryRunner) {
     await queryRunner.query(
-      `CREATE TABLE "user" ("id" integer PRIMARY KEY SERIAL NOT NULL, "email" varchar NOT NULL, "password" varchar NOT NULL, "admin" boolean NOT NULL DEFAULT (1))`,
+      `CREATE TABLE "user" ("id" SERIAL PRIMARY KEY, "email" varchar NOT NULL, "password" varchar NOT NULL, "admin" boolean NOT NULL DEFAULT (1))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "report" ("id" integer PRIMARY KEY SERIAL NOT NULL, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer)`,
+      `CREATE TABLE "report" ("id" SERIAL PRIMARY KEY, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer)`,
     );
     await queryRunner.query(
-      `CREATE TABLE "temporary_report" ("id" integer PRIMARY KEY SERIAL NOT NULL, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer, CONSTRAINT "FK_e347c56b008c2057c9887e230aa" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
+      `CREATE TABLE "temporary_report" ("id" SERIAL PRIMARY KEY, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer, CONSTRAINT "FK_e347c56b008c2057c9887e230aa" FOREIGN KEY ("userId") REFERENCES "user" ("id") ON DELETE NO ACTION ON UPDATE NO ACTION)`,
     );
     await queryRunner.query(
       `INSERT INTO "temporary_report"("id", "price", "make", "model", "year", "lng", "lat", "mileage", "approved", "userId") SELECT "id", "price", "make", "model", "year", "lng", "lat", "mileage", "approved", "userId" FROM "report"`,
@@ -27,7 +27,7 @@ module.exports = class initialSchema1655226837451 {
       `ALTER TABLE "report" RENAME TO "temporary_report"`,
     );
     await queryRunner.query(
-      `CREATE TABLE "report" ("id" integer PRIMARY KEY SERIAL NOT NULL, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer)`,
+      `CREATE TABLE "report" ("id" SERIAL PRIMARY KEY, "price" integer NOT NULL, "make" varchar NOT NULL, "model" varchar NOT NULL, "year" integer NOT NULL, "lng" integer NOT NULL, "lat" integer NOT NULL, "mileage" integer NOT NULL, "approved" boolean NOT NULL DEFAULT (0), "userId" integer)`,
     );
     await queryRunner.query(
       `INSERT INTO "report"("id", "price", "make", "model", "year", "lng", "lat", "mileage", "approved", "userId") SELECT "id", "price", "make", "model", "year", "lng", "lat", "mileage", "approved", "userId" FROM "temporary_report"`,
