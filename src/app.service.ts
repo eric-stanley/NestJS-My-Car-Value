311e3e62f388746752da0f2f22f148979fd5935d
Eric Stanley <ericstanley@pop-os.localdomain>
2022-06-16 09:35:00 +0530

    home route return text

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'NestJS API for calculating car price estimates with authorization and authentication';
   }
 }
