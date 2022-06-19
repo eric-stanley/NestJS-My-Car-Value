766ec9f899b75bd029af493196dcfa9cd776fa94
Eric Stanley <ericstanley@pop-os.localdomain>
2022-06-19 22:37:50 +0530

    minor path changes in e2e spec file

--- test/app.e2e.spec.ts
+++ test/app.e2e.spec.ts
@@ -19,6 +19,8 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect(
+        'NestJS API for calculating car price estimates with authorization and authentication',
+      );
   });
 });
--- test/test.setup.ts
+++ test/test.setup.ts
@@ -4,7 +4,7 @@ import { getConnection } from 'typeorm';
 
 global.beforeEach(async () => {
   try {
-    await rm(join(__dirname, '..', 'db.test.sqlite'));
+    await rm(join(__dirname, '..', 'db', 'db.test.sqlite'));
   } catch (err) {}
 });
 
