e96052a8724c8a356f653588637303a48657a671
Eric Stanley <ericstanley@pop-os.localdomain>
2022-06-26 22:23:53 +0530

    updated heroku config in config service

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -21,22 +21,35 @@ class ConfigService {
 
   public getTypeORMConfig(): TypeOrmModuleOptions {
     const baseDir = path.join(__dirname, '../');
-    const entitiesPath = `${baseDir}${this.envConfig.TYPEORM_ENTITIES}`;
-    const migrationPath = `${baseDir}${this.envConfig.TYPEORM_MIGRATIONS}`;
+    const entitiesPath = `${baseDir}${
+      this.envConfig.TYPEORM_ENTITIES || process.env.TYPEORM_ENTITIES
+    }`;
+    const migrationPath = `${baseDir}${
+      this.envConfig.TYPEORM_MIGRATIONS || process.env.TYPEORM_MIGRATIONS
+    }`;
 
-    const type: any = this.envConfig.TYPEORM_CONNECTION;
     let options = {
-      type,
-      host: this.envConfig.TYPEORM_HOST,
-      username: this.envConfig.TYPEORM_USERNAME,
-      password: this.envConfig.TYPEORM_PASSWORD,
-      database: this.envConfig.TYPEORM_DATABASE,
-      port: Number.parseInt(this.envConfig.TYPEORM_PORT, 10),
-      logger: this.envConfig.TYPEORM_LOGGER as any,
-      logging: this.envConfig.TYPEORM_LOGGING === 'true',
+      type:
+        this.envConfig.TYPEORM_CONNECTION ||
+        (process.env.TYPEORM_CONNECTION as any),
+      host: this.envConfig.TYPEORM_HOST || process.env.TYPEORM_HOST,
+      username: this.envConfig.TYPEORM_USERNAME || process.env.TYPEORM_USERNAME,
+      password: this.envConfig.TYPEORM_PASSWORD || process.env.TYPEORM_PASSWORD,
+      database: this.envConfig.TYPEORM_DATABASE || process.env.TYPEORM_DATABASE,
+      port: Number.parseInt(
+        this.envConfig.TYPEORM_PORT || process.env.TYPEORM_PORT,
+        10,
+      ),
+      logger:
+        this.envConfig.TYPEORM_LOGGER || (process.env.TYPEORM_LOGGER as any),
+      logging:
+        this.envConfig.TYPEORM_LOGGING ||
+        process.env.TYPEORM_LOGGING === 'true',
       entities: [entitiesPath],
       migrations: [migrationPath],
-      migrationsRun: this.envConfig.TYPEORM_MIGRATIONS_RUN === 'true',
+      migrationsRun:
+        this.envConfig.TYPEORM_MIGRATIONS_RUN ||
+        process.env.TYPEORM_MIGRATIONS_RUN === 'true',
       cli: {
         migrationsDir: 'src/db/migrations',
         entitiesDir: 'src/**/entities',
